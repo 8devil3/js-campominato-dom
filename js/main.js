34cbefcd4ddcd7673fe934930512c4304c26796b
8devil3 <8dev.il3@gmail.com>
2022-03-03 16:01:41 +0100

    aggiunto event listener bombe

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
+/* L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+BONUS:
+1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste */
+
+
+
 const divContainer = document.querySelector(".container");
 const selectLevelInput = document.querySelector("#level");
 const btnPlay = document.querySelector("#play");
 const btnReset = document.querySelector("#reset");
 
-const lvEasy = 100; //livello difficile
+const lvEasy = 100; //livello facile
 const lvMedium = 81; //livello medio
-const lvHard = 49; //livello facile
+const lvHard = 49; //livello difficile
 
-const bombsNumber = 16; //quantità bombe
+const bombsNumber = 2; //quantità bombe
 
 const widthHeightNumberBox = 40; //misura lato del singolo box dei numeri
 
@@ -28,13 +44,10 @@ function play() { // avvio gioco
     
     if (selectLevelInput.value == '1') { //generazione griglia di gioco
         grid(lvEasy);
-        console.log(bombsGenerator(lvEasy))
     } else if (selectLevelInput.value == '2') {
         grid(lvMedium);
-        console.log(bombsGenerator(lvMedium));
     } else {
         grid(lvHard);
-        console.log(bombsGenerator(lvHard));
     }
 }
 
@@ -43,6 +56,9 @@ function play() { // avvio gioco
 function grid(level) { //generatore della griglia, arg -> int
     
     let divNumberBox;
+    let bombs = bombsGenerator(level);
+
+    console.log(bombs);
 
     for (let x = 1; x <= level; x++) {
         divNumberBox = document.createElement("div");
@@ -55,12 +71,20 @@ function grid(level) { //generatore della griglia, arg -> int
 
         divContainer.style.width = widthHeightNumberBox * Math.sqrt(level) + "px";
         divContainer.style.height = widthHeightNumberBox * Math.sqrt(level) + "px";
+
+        divNumberBox.addEventListener('click', function(){
+            if (bombs.includes(parseInt(this.innerHTML))) {
+                this.classList.add('false');
+            } else {
+                this.classList.add('true');
+            }
+        });
     }
 
 }
 
 
-function bombsGenerator(level) { //generazione casuale delle bombe
+function bombsGenerator(level) { //generazione casuale delle bombe, arg -> int
 
     const arrBombs = [];
     let bomb;
@@ -72,9 +96,23 @@ function bombsGenerator(level) { //generazione casuale delle bombe
         while (arrBombs.includes(bomb)) {
             bomb = Math.floor(Math.random() * level) + 1;
         }
-        
+
         arrBombs.push(bomb);
     }
 
     return arrBombs;
 }
+
+
+
+
+
+
+// function checkBomb() {
+          
+//     if (arrBombs.includes(parseInt(this.innerHTML))) {
+//         this.classList.add('true');
+//     } else {
+//         this.classList.add('false');
+//     }
+// }
